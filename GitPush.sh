b2d08dbfdab5fdd8992db82c1545d62e5bf80c01
luo xiaodong <275611813@qq.com>
2021-01-20 22:10:41 +0800

    update, commit: LXD, commit time: 2021-01-20 22:10:35

--- GitPush.sh
+++ GitPush.sh
@@ -5,14 +5,14 @@ echo "放弃有道云的利器 GitAutoPush Starting..."
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git add .
 
-read -t 30 -p "请输入提交注释:" msg
+read -t 30 -p "content:" msg
 
 if  [ ! "$msg" ] ;then
     echo "[commit message] 默认提交, 提交人: $(whoami), 提交时间: ${time}"
-	git commit -m "默认提交, 提交人: $(whoami), 提交时间: ${time}"
+	git commit -m "commit: $(whoami), commit time: ${time}"
 else
     echo "[commit message] $msg, 提交人: $(whoami), 提交时间: ${time}"
-	git commit -m "$msg, 提交人: $(whoami), 提交时间: ${time}"
+	git commit -m "$msg, commit: $(whoami), commit time: ${time}"
 fi
 
 	
